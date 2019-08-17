78683765b7d9d83b6856cdc7c1996a03034a4196
LEE HANYONG <glhy0718@gmail.com>
2019-08-17 18:57:30 +0900

    no message

---
+++ SpaceTollgate/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    private PlayerController player;
+    void Start() {
+        player = GameManager.instance.player;
+    }
+    void Update() {
+        if (player != null)
+            transform.position = player.transform.position + new Vector3(0, 0, -10);
+
+    }
+}
