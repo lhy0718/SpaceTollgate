007c74a0fe9a32a74fadad1b197e7d228cd811f6
LEE HANYONG <glhy0718@gmail.com>
2019-08-18 03:36:42 +0900

    main씬에 미니맵 추가

---
+++ SpaceTollgate/Assets/Scripts/MinimapCamera.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    void Update() {
+        transform.position = GameManager.instance.player.transform.position + new Vector3(0, 0, -2);
+    }
+}
