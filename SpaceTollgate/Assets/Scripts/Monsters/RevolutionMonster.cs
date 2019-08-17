c4c1d7bce8c6f2d84158f093b153a1596e0a8f9e
18신대성 <esc990720@korea.ac.kr>
2019-08-18 01:48:18 +0900

    공전하는 몬스터도 만듬

---
+++ SpaceTollgate/Assets/Scripts/Monsters/RevolutionMonster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RevolutionMonster : Monster
+{
+    public bool isClockwise = true;
+    public float speed = 1;
+    public float radius = 3;
+
+    private Vector2 axisPoint;
+    private Rigidbody2D rb;
+    public override void BeforeDestroy()
+    {
+        return;
+    }
+
+    private void Awake()
+    {
+        monsterType = MonsterType.Revolution;
+    }
+
+    private void Start()
+    {
+        axisPoint = transform.position;
+        transform.position = axisPoint + new Vector2(0, radius);
+    }
+
+    private void Update()
+    {
+        transform.RotateAround(axisPoint, new Vector3(0,0,1), Time.deltaTime * speed);
+    }
+}
