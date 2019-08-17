09bddaca98d82a540cb49b120cfd139927b0a6ee
LEE HANYONG <glhy0718@gmail.com>
2019-08-17 22:55:18 +0900

    enemy finder 구현

--- SpaceTollgate/Assets/Scripts/GameManager.cs
+++ SpaceTollgate/Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
     public static GameManager instance;
 
+    public new Camera camera;
     public Text text;
     public Text scoreText;
     public Button retryButton;
